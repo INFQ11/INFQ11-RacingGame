17847a055f3989550d2d8551eecbacaac02a36fe
EmbMax <m.emberger77@gmail.com>
2023-05-03 18:47:12 +0200

    Background + Picture now works + StartButton switches to next Bildschirm

--- Startbildschirm.java
+++ Startbildschirm.java
@@ -4,13 +4,13 @@ public class Startbildschirm extends Bildschirm
 {
     public Startbildschirm()
     {
-        buttons[0].isActive = true;
-        wurzel.add(buttons[0]);
-        
         int breite = myGame.BREITE;
         int höhe = myGame.HOEHE;
         
         Bild bild = new Bild(0,0,breite, höhe,"D:\\Git Workspace\\Q11_Informatik_Template_Startmenü\\pictures\\Background.jpg");
         wurzel.add(bild);
+        
+        buttons[0].isActive = true;
+        wurzel.add(buttons[0]);
     }
 }
