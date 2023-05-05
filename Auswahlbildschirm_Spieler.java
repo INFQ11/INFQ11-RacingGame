130ceffeb7ef013900b137078626446154030892
INFQ11 <q11informatik2023@gmail.com>
2023-05-05 18:28:56 +0200

    constr a_s 2

--- Auswahlbildschirm_Spieler.java
+++ Auswahlbildschirm_Spieler.java
@@ -7,6 +7,19 @@ public class Auswahlbildschirm_Spieler extends Bildschirm
 {
     public Auswahlbildschirm_Spieler()
     {
+        int breite = myGame.BREITE;
+        int höhe = myGame.HOEHE;
         
+        Bild bild = new Bild(0,0,breite, höhe,"D:\\Git Workspace\\Q11_Informatik_Template_Startmenü\\pictures\\Background.jpg");
+        wurzel.add(bild);
+        
+        /**
+         * Information: Buttons müssen nun nicht mehr in ButtonSammlung erzeugt
+         * oder angemeldet werden!
+         */
+        
+        OKButton okb = new OKButton(0,0,0,"");
+        okb.isActive = true;
+        wurzel.add(okb);
     }
 }
