19cbd602c65d12b17da1633944068fca87cd8bac
ge92huj <max.emberger@tum.de>
2023-07-10 17:19:30 +0200

    sound fixed

--- Hilfe.java
+++ Hilfe.java
@@ -12,21 +12,21 @@ public class Hilfe extends Bildschirm
         Line[1] = "";
         Line[2] = "Mit Verlaub bedanken wir uns bei Ihnen, dass Sie sich dazu entschieden haben, dieses Spiel zu selegieren.";
         Line[3] = "Im Folgenden wird Ihnen eine Instruktion dargeboten, mit welcher Sie es vermögen werden folgendes Spiel in Genuss zu erfahren.";
-        Line[4] = "I. Um Ihre Computer-Maus zu mobilisieren, bitten wir Sie hochachtungsvoll auf Ihrem Tastenwerk die Taste „M“ zu erlesen.";
-        Line[5] = "II. Anschließend wird Ihnen die Möglichkeit präsentiert, den Startbutton zu betätigen.";
-        Line[6] = "III. Ist diese Hürde genommen, stellt sich Ihnen die Chance dar, die Spieleranzahl mit den Auswahlflächen „Einzelspieler“ und „Mehrspieler“ zu bestimmen.";
-        Line[7] = "IV. Ist dies gemeistert, werden Sie förmlichst dazu aufgefordert, einen Magister zu bestimmen, der Ihr Verlangen stimuliert. Dies können Sie wahrnehmen, ";
+        Line[4] = "I.) Um Ihre Computer-Maus zu mobilisieren, bitten wir Sie hochachtungsvoll auf Ihrem Tastenwerk die Taste „M“ zu erlesen.";
+        Line[5] = "II.) Anschließend wird Ihnen die Möglichkeit präsentiert, den Startbutton zu betätigen.";
+        Line[6] = "III.) Ist diese Hürde genommen, stellt sich Ihnen die Chance dar, die Spieleranzahl mit den Auswahlflächen „Einzelspieler“ und „Mehrspieler“ zu bestimmen.";
+        Line[7] = "IV.) Ist dies gemeistert, werden Sie förmlichst dazu aufgefordert, einen Magister zu bestimmen, der Ihr Verlangen stimuliert. Dies können Sie wahrnehmen, ";
         Line[8] = "indem Sie auf die bereitgestellte Hilfe der „Pass-Taste“ zugreifen. Durch die Betätigung von jener werden Ihnen zahlreiche Auswahlmöglichkeiten vorgeführt unter jenen Sie wählen können.";
         Line[9] = "Ist Ihr Votum abgeschlossen wird Ihnen die Gelegenheit bereitgestellt durch Betätigen der „Smash-Taste“ Ihre Wahl zu untermauern.";
-        Line[10] = "V. Darauffolgend sollten Sie visuell wahrnehmen, dass Sie sich mit Ihrem ausgewählten Personenkraftwagen auf einer Startposition einer Rennstrecke wiederfinden.";
+        Line[10] = "V.) Darauffolgend sollten Sie visuell wahrnehmen, dass Sie sich mit Ihrem ausgewählten Personenkraftwagen auf einer Startposition einer Rennstrecke wiederfinden.";
         Line[11] = "Nach der Realisierung der Situation wird ein Countdown in Ihr Sichtfeld geraten.";
-        Line[12] = "VI. Ist dieser abgelaufen, wird Ihnen die Möglichkeit bereitgestellt durch das Betätigen gewisser Tasten Ihr Renngefährt zu steuern.";
+        Line[12] = "VI.) Ist dieser abgelaufen, wird Ihnen die Möglichkeit bereitgestellt durch das Betätigen gewisser Tasten Ihr Renngefährt zu steuern.";
         Line[13] = "Um Geschwindigkeit zu erreichen ist es von Nöten die Taste „W“ zu betätigen. Verspüren Sie jedoch den Wunsch Ihr Gefährt zu verlangsamen oder in gegenteilige Richtung zu beschleunigen,";
         Line[14] = "legen wir Ihnen nahe „S“ zu betätigen. Möchten Sie die Ausrichtung des Fahrzeugs ändern, können Sie das mit „A“ und „D“ realisieren.";
-        Line[15] = "VII. Während Sie sich im Wettstreit befinden, werden immer wieder herausfordernde Fragen auf Ihrem Bildschirm erscheinen. Kurze Zeit später erscheinen auf der Strecke drei Auswahlmöglichkeiten,";
+        Line[15] = "VII.) Während Sie sich im Wettstreit befinden, werden immer wieder herausfordernde Fragen auf Ihrem Bildschirm erscheinen. Kurze Zeit später erscheinen auf der Strecke drei Auswahlmöglichkeiten,";
         Line[16] = "mit welchen Sie Ihre Antwort unterstreichen können. Haben Sie die Leistung vollbracht die korrekte Antwort zu bestimmen, erhalten sie einen Zähler.";
         Line[17] = "Versagen Sie jedoch bleibt Ihnen diese Leistung verweigert.";
-        Line[18] = "VIII. Nachdem der Rennspaß zu neige gegangen ist, wird der Spieler ausgezeichnet, dessen Zeit und erreichte Punkte in der Gesamtwertung überlegen sind.";
+        Line[18] = "VIII.) Nachdem der Rennspaß zu neige gegangen ist, wird der Spieler ausgezeichnet, dessen Zeit und erreichte Punkte in der Gesamtwertung überlegen sind.";
         Line[19] = "";
         Line[20] = "Der gesamte Informatikkurs wünscht Ihnen VIEL FREUDE beim Exerzieren dieses Produktes.";
         
@@ -85,7 +85,7 @@ public class Hilfe extends Bildschirm
         ButtonFacade.getButton(ButtonType.WEITER).setActive(true);
         wurzel.add(ButtonFacade.getButton(ButtonType.WEITER));
         
-        menu.playMenu();
+        //menu.playMenu();
 
     }
 
