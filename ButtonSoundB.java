98ca642bbd2a15113a7543d86d9ad46c8e1c95e2
INFQ11 <q11informatik2023>
2023-07-10 11:30:50 +0200

    more sounds

--- ButtonSoundB.java
+++ ButtonSoundB.java
@@ -3,8 +3,8 @@ public class ButtonSoundB
 {
     public static void playBSB()
     {
-        Sound ButtonSoundB = new Sound("Resources/Sounds/ButtonSoundB.wav");
-        ButtonSoundB.play();
+        Sound BSB = new Sound("Resources/Sounds/ButtonSoundB.wav");
+        BSB.play();
     }
     public ButtonSoundB(){}
 }
