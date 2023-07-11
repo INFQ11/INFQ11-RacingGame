915944b792a23b2a873230d19ed0287b3e872309
unknown <SilKon@bsg-koetzting.schule>
2023-07-11 07:48:22 +0200

    end

--- Einzelspieler.java
+++ Einzelspieler.java
@@ -20,32 +20,32 @@ public class Einzelspieler extends Bildschirm
         Rechteck WandVert1 = new Rechteck(610,560,10,2640);
         obstacles.add(WandVert1);
         WandVert1.passivMachen();
-        wurzel.add(WandVert1);
+        
 
         Rechteck WandHor1 = new Rechteck(660,500,405,10);
         obstacles.add(WandHor1);
         WandHor1.passivMachen();
-        wurzel.add(WandHor1);
+        
 
         Rechteck WandHor2 = new Rechteck(610,720,630,10);
         obstacles.add(WandHor2);
         WandHor2.passivMachen();
-        wurzel.add(WandHor2);
+        
 
         Rechteck WandHor3 = new Rechteck(1360,1060,410,10);
         obstacles.add(WandHor3);
         WandHor3.passivMachen();
-        wurzel.add(WandHor3);
+        
 
         Rechteck WandHor4 = new Rechteck(610,1440,700,10);
         obstacles.add(WandHor4);
         WandHor4.passivMachen();
-        wurzel.add(WandHor4);
+        
 
         Rechteck WandHor5 = new Rechteck(950,2000,1750,10);
         obstacles.add(WandHor5);
         WandHor5.passivMachen();
-        wurzel.add(WandHor5);
+        
 
         Rechteck WandVert2 = new Rechteck(1770,0,10,2000);
         obstacles.add(WandVert2);
@@ -55,82 +55,82 @@ public class Einzelspieler extends Bildschirm
         Rechteck WandHor6 = new Rechteck(2700,1685,270,10);
         obstacles.add(WandHor6);
         WandHor6.passivMachen();
-        wurzel.add(WandHor6);
+        
 
         Rechteck WandHor7 = new Rechteck(2285,785,270,10);
         obstacles.add(WandHor7);
         WandHor7.passivMachen();
-        wurzel.add(WandHor7);
+        
 
         Rechteck WandHor8 = new Rechteck(2560,500,2665,10);
         obstacles.add(WandHor8);
         WandHor8.passivMachen();
-        wurzel.add(WandHor8);
+        
 
         Rechteck WandVert3 = new Rechteck(3450,500,10,1780);
         obstacles.add(WandVert3);
         WandVert3.passivMachen();
-        wurzel.add(WandVert3);
+        
 
         Rechteck WandHor9 = new Rechteck(3975,1337,1790,10);
         obstacles.add(WandHor9);
         WandHor9.passivMachen();
-        wurzel.add(WandHor9);
+        
 
         Rechteck WandVert4 = new Rechteck(5225,500,10,440);
         obstacles.add(WandVert4);
         WandVert4.passivMachen();
-        wurzel.add(WandVert4);
+        
 
         Rechteck WandVert5 = new Rechteck(4490,820,10,1150);
         obstacles.add(WandVert5);
         WandVert5.passivMachen();
-        wurzel.add(WandVert5);
+        
 
         Rechteck WandHor10 = new Rechteck(4490,1730,200,10);
         obstacles.add(WandHor10);
         WandHor10.passivMachen();
-        wurzel.add(WandHor10);
+        
 
         Rechteck WandHor11 = new Rechteck(620,2280,4540,10);
         obstacles.add(WandHor11);
         WandHor11.passivMachen();
-        wurzel.add(WandHor11);
+        
 
         Rechteck WandVert6 = new Rechteck(5150,1790,10,1350);
         obstacles.add(WandVert6);
         WandVert6.passivMachen();
-        wurzel.add(WandVert6);
+        
 
         Rechteck WandHor12 = new Rechteck(3890,3220,1140,10);
         obstacles.add(WandHor12);
         WandHor12.passivMachen();
-        wurzel.add(WandHor12);
+        
 
         Rechteck WandHor13 = new Rechteck(3750,2950,200,10);
         obstacles.add(WandHor13);
         WandHor13.passivMachen();
-        wurzel.add(WandHor13);
+        
 
         Rechteck WandHor14 = new Rechteck(3260,2620,550,10);
         obstacles.add(WandHor14);
         WandHor14.passivMachen();
-        wurzel.add(WandHor14);
+        
 
         Rechteck WandVert7 = new Rechteck(3525,2430,10,1180);
         obstacles.add(WandVert7);
         WandVert7.passivMachen();
-        wurzel.add(WandVert7);
+        
 
         Rechteck WandHor15 = new Rechteck(3040,2950,290,10);
         obstacles.add(WandHor15);
         WandHor15.passivMachen();
-        wurzel.add(WandHor15);
+        
 
         Rechteck WandHor16 = new Rechteck(610,3220,2590,10);
         obstacles.add(WandHor16);
         WandHor16.passivMachen();
-        wurzel.add(WandHor16);
+        
         
         Rechteck WandVert7b = new Rechteck(3534,2450,31,1160);
         obstacles.add(WandVert7b);
--- Ende.java
+++ Ende.java
@@ -8,25 +8,26 @@ public class Ende extends Bildschirm
     public Ende()
     {
         myGame.getInstance().getCam().positionSetzen(new Punkt(0,0));
-        //Bild Hintergrund = new Bild(0,0,100,TexturManager.DATEIPFAD + "EndBildschirm.png");
+        
         
         newArray();
-        Text Überschrift = new Text(300,200,50,Line[0]);
+        Text Überschrift = new Text(100,200,50,Line[0]);
         wurzel.add(Überschrift);
-        Text t1 = new Text(200,300,20,Line[1]);
+        Text t1 = new Text(100,300,20,Line[1]);
         wurzel.add(t1);
-        Text t2 = new Text(200,340,20,Line[2]);
+        Text t2 = new Text(100,340,20,Line[2]);
         wurzel.add(t2);
-        Text t3 = new Text(200,380,20,Line[3]);
+        Text t3 = new Text(100,380,20,Line[3]);
         wurzel.add(t3);
-        Text t4 = new Text(200,420,20,Line[4]);
+        Text t4 = new Text(100,420,20,Line[4]);
         wurzel.add(t4);
-        Text t5 = new Text(200,460,20,Line[5]);
+        Text t5 = new Text(100,460,20,Line[5]);
         wurzel.add(t5);
         Text score = new Text(600,120,50,Line[6]);
         wurzel.add(score);
-        Text info = new Text(200, 500, 20, Line[7]);
+        Text info = new Text(100, 500, 20, Line[7]);
         wurzel.add(info);
+        
     }
 
     public void newArray()
